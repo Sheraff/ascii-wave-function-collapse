f3cdd6864eba8b486a92805ba781ccd5500901e5
Sheraff <fpellet@ensc.fr>
2022-07-07 00:48:51 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,170 @@
+import TILES from './tiles.js'
+const GRID_SIZE = [50, 20]
+const NEIGHBORS = [
+	[0, -1],
+	[1, 0],
+	[0, 1],
+	[-1, 0],
+]
+
+const pre = document.querySelector('pre')
+if(!pre)
+	throw new Error('No pre element found')
+
+void function(pre) {
+	const array = createNewGrid(GRID_SIZE)
+	const backtrack = {
+		cursor: 1,
+		stack: [],
+	}
+	loop(pre, array, backtrack)
+}(pre)
+
+async function loop(pre, array, backtrack) {
+	const cells = getMinEntropyCells(array)
+	if (cells.length === 0)
+		return
+	const randomCell = cells[Math.floor(Math.random() * cells.length)]
+	try {
+		const randomOption = findSuitableOption(array, randomCell, backtrack)
+		randomCell.tile = randomOption
+		collapseNeighbors(array, randomCell)
+		draw(pre, array)
+		await frame()
+		loop(pre, array, backtrack)
+	} catch (error) {
+		if (error instanceof Error)
+			throw error
+		console.log('backtracking', backtrack.cursor)
+		loop(pre, error, backtrack)
+	}
+}
+
+function frame() {
+	return new Promise(resolve => requestAnimationFrame(resolve))
+}
+
+function collapseNeighbors(array, cell, stack = []) {
+	for (let i = 0; i < NEIGHBORS.length; i++) {
+		const vector = NEIGHBORS[i]
+		const x = cell.x + vector[0]
+		const y = cell.y + vector[1]
+		if (x < 0 || x >= GRID_SIZE[0] || y < 0 || y >= GRID_SIZE[1])
+			continue
+		const neighbor = getCellFromXY(array, x, y)
+		if (neighbor.tile)
+			continue
+		const possibilities = cell.tile
+			? [cell.tile.is[i]]
+			: cell.accepts[i]
+		const mirrorIndex = (i + 2) % 4
+		const compatibleOptions = neighbor.accepts[mirrorIndex].filter(
+			option => possibilities.includes(option)
+		)
+		if (compatibleOptions.length === 0) {
+			console.log({neighbor, cell})
+			throw new Error('Invalid neighbor')
+		}
+		if (neighbor.accepts[mirrorIndex].length !== compatibleOptions.length)
+			stack.push(neighbor)
+		neighbor.accepts[mirrorIndex] = compatibleOptions
+		neighbor.entropy = neighbor.accepts.reduce(
+			(sum, options) => sum + options.length,
+			0
+		)
+		// if (neighbor.entropy === 4)
+		// 	neighbor.tile = findSuitableOption(array, neighbor)[0]
+	}
+	if (stack.length > 0) {
+		const nextCell = stack.shift()
+		collapseNeighbors(array, nextCell, stack)
+	}
+}
+
+function findSuitableOption(array, cell, backtrack) {
+	const options = TILES.filter(option => 
+		cell.accepts.every(
+			(directionalOption, i) => directionalOption.includes(option.is[i])
+		)
+	)
+
+	if (options.length === 0) {
+		if(backtrack.stack.length > 0) {
+			const revertCount = Math.min(Math.floor(backtrack.cursor), backtrack.stack.length)
+			const [reset] = backtrack.stack.splice(backtrack.stack.length - revertCount)
+			backtrack.cursor += 1
+			throw JSON.parse(reset)
+		}
+		console.log({cell, options})
+		throw new Error('No suitable options')
+	}
+
+	backtrack.stack.push(JSON.stringify(array))
+	backtrack.cursor = Math.max(1, backtrack.cursor - 0.1)
+	
+	const randomOption = options[Math.floor(Math.random() * options.length)]
+	return randomOption
+}
+
+function createNewGrid([width, height]) {
+	return new Array(width * height).fill(0).map((_, index, array) => {
+		const [x, y] = getXYFromIndex(array, index)
+		const top    = y === 0          ? [0] : [0,1,2,3]
+		const right  = x === width - 1  ? [0] : [0,1,2,3]
+		const bottom = y === height - 1 ? [0] : [0,1,2,3]
+		const left   = x === 0          ? [0] : [0,1,2,3]
+		const entropy = top.length + right.length + bottom.length + left.length
+		return ({
+			index,
+			x,
+			y,
+			entropy,
+			accepts: [
+				top,
+				right,
+				bottom,
+				left,
+			],
+			tile: null,
+		})
+	})
+}
+
+function getMinEntropyCells(array) {
+	let min = Infinity
+	let cells = []
+	for(let i = 0; i < array.length; i++) {
+		const cell = array[i]
+		if (cell.tile)
+			continue
+		if (cell.entropy < min) {
+			min = cell.entropy
+			cells = [cell]
+		} else if (cell.entropy === min) {
+			cells.push(cell)
+		}
+	}
+	return cells
+}
+
+function draw(pre, array) {
+	let text = ''
+	for(let y = 0; y < GRID_SIZE[1]; y++) {
+		for(let x = 0; x < GRID_SIZE[0]; x++) {
+			const cell = getCellFromXY(array, x, y)
+			text += cell.tile?.symbol || '~'
+		}
+		text += '\n'
+	}
+	pre.textContent = text
+}
+
+function getXYFromIndex(array, i) {
+	const x = i % GRID_SIZE[0]
+	const y = Math.floor(i / GRID_SIZE[0])
+	return [x, y]
+}
+
+function getCellFromXY(array, x, y) {
+	return array[y * GRID_SIZE[0] + x]
+}
